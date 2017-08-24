26f89b83dbea8a0b1a89e4665beec3264c5ac7cb
Optic969 <optic969@tut.by>
2017-08-25 01:15:58 +0300

    Test1

---
+++ src/SIMPLNumbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SIMPLNumbers {
+
+
+        public static void main(String[] args) {
+            countSimpleNumber();
+        }
+
+
+        private static void countSimpleNumber() {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите число N: ");
+            int n = in.nextInt();
+            System.out.println("Простые числа от 1 до " + n);
+            int num, arg;
+            for (num = 2; num <= n; num++) {
+                arg = 2;
+                while ((num % arg) > 0) {
+                    arg++;
+                }
+                if (arg == num) System.out.print(num + "\t");
+            }
+
+        }
+    }
+
